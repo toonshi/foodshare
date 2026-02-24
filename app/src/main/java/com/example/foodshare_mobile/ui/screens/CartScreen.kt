00099e58c773e5c9d7fa48d090eaff0590a4bd2c
Mike Agoya <agoyamike@gmail.com>
2026-02-24 16:46:00 +0300

    Fix: Re-add missing background import in CartScreen.kt

--- app/src/main/java/com/example/foodshare_mobile/ui/screens/CartScreen.kt
+++ app/src/main/java/com/example/foodshare_mobile/ui/screens/CartScreen.kt
@@ -1,5 +1,6 @@
 package com.example.foodshare_mobile.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
